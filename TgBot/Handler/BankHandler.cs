900fcaecf026d5adb302163b5a9dd1fd839ff345
Vadim Petrulenko <66614@gmail.com>
2024-02-03 12:33:15 +0300

    add links

--- TgBot/Handler/BankHandler.cs
+++ TgBot/Handler/BankHandler.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TgBot.Handler
 {
   public static class BankHandler
   {
-     
+     async han
     
   }
 }
