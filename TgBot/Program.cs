01c4f98412667bb8d36f0cc9cdc86722e0ea5e37
Kakelys <alexglobal1459@gmail.com>
2024-02-03 12:10:12 +0300

    add base bot handler

--- TgBot/Program.cs
+++ TgBot/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var bot = new TelegramBotClient("6925511900:AAFIkCLTgwRyPcS27u2avPs6GainQHHduao");
+
+async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+}
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+}
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = new UpdateType[]{UpdateType.Message}, // only recieve new messages
+};
+
+bot.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken
+);
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
