8413f2309ed261ffc0c17f80e7a3621c0bc546a5
Vadim Petrulenko <66614@gmail.com>
2024-02-03 12:20:45 +0300

    Create Enums handler

---
+++ TgBot/Enums/BankWorker.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgBot.Enums
+{
+  public enum BankWorker
+  {
+        Getbanks,
+        GetListCurrency,
+        Rate,
+        Rates,
+        Statistics
+  }
+}
